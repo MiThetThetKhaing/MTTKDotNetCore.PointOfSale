277937ce1587cf38a8f90727fb9e9f37fc7ceb9c
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2024-12-12 00:21:29 +0630

    feat: product category API CRUD

--- MTTKDotNetCore.PointOfSale.API/Controllers/Endpoints/ProductCategoryController.cs
+++ MTTKDotNetCore.PointOfSale.API/Controllers/Endpoints/ProductCategoryController.cs
@@ -22,5 +22,33 @@ namespace MTTKDotNetCore.PointOfSale.API.Controllers.Endpoints
             var result = await _productCategoryService.CreateProductCategory(productCategory);
             return Execute(result);
         }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var result = await _productCategoryService.GetAllCategories();
+            return Execute(result);
+        }
+
+        [HttpGet("{categoryCode}")]
+        public async Task<IActionResult> GetCategory(string categoryCode)
+        {
+            var result = await _productCategoryService.GetCategoryByCode(categoryCode);
+            return Execute(result);
+        }
+
+        [HttpPatch("{categoryCode}")]
+        public async Task<IActionResult> UpdateCategory(string categoryCode, string newName)
+        {
+            var result = await _productCategoryService.ChangeCategoryName(categoryCode, newName);
+            return Execute(result);
+        }
+
+        [HttpDelete("{categoryCode}")]
+        public async Task<IActionResult> DeleteCategory(string categoryCode)
+        {
+            var result = await _productCategoryService.DeleteCategory(categoryCode);
+            return Execute(result);
+        }
     }
 }
