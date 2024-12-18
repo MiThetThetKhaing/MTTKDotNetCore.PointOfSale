2cd073d004238c1a04d1fbcd3ed9060a304ddf83
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2024-12-18 15:40:14 +0630

    feat: try catch in product category controller

--- MTTKDotNetCore.PointOfSale.API/Controllers/Endpoints/ProductCategoryController.cs
+++ MTTKDotNetCore.PointOfSale.API/Controllers/Endpoints/ProductCategoryController.cs
@@ -19,36 +19,71 @@ namespace MTTKDotNetCore.PointOfSale.API.Controllers.Endpoints
         [HttpPost("create")]
         public async Task<IActionResult> CreateProductCategory(TblProductCategoryPos productCategory)
         {
-            var result = await _productCategoryService.CreateProductCategory(productCategory);
-            return Execute(result);
+            try
+            {
+                var result = await _productCategoryService.CreateProductCategory(productCategory);
+                return Execute(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpGet()]
         public async Task<IActionResult> GetAllCategories()
         {
-            var result = await _productCategoryService.GetAllCategories();
-            return Execute(result);
+            try
+            {
+                var result = await _productCategoryService.GetAllCategories();
+                return Execute(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpGet("{categoryCode}")]
         public async Task<IActionResult> GetCategory(string categoryCode)
         {
-            var result = await _productCategoryService.GetCategoryByCode(categoryCode);
-            return Execute(result);
+            try
+            {
+                var result = await _productCategoryService.GetCategoryByCode(categoryCode);
+                return Execute(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPatch("{categoryCode}")]
         public async Task<IActionResult> UpdateCategory(string categoryCode, string newName)
         {
-            var result = await _productCategoryService.ChangeCategoryName(categoryCode, newName);
-            return Execute(result);
+            try
+            {
+                var result = await _productCategoryService.ChangeCategoryName(categoryCode, newName);
+                return Execute(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpDelete("{categoryCode}")]
         public async Task<IActionResult> DeleteCategory(string categoryCode)
         {
-            var result = await _productCategoryService.DeleteCategory(categoryCode);
-            return Execute(result);
+            try
+            {
+                var result = await _productCategoryService.DeleteCategory(categoryCode);
+                return Execute(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
